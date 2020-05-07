ab178e3d098515ba0dde422064f2c4a93395eeeb
Emeka Udeh <udehemeka.edu@gmail.com>
2020-05-07 13:00:17 +0200

    Update globals.py

--- globals.py
+++ globals.py
@@ -9,3 +9,21 @@ Q_TRAIN = 15
 Q_TEST = 1
 LSTM_LAYERS = 1
 UNROLLING_STEPS = 2
+
+for obj in classes_shots.keys():
+            # add the query image to each object 
+            images.append({
+                        'dataset': dataset,
+                        'class_name': obj,
+                        'file_name': next(iter(query[obj].keys())),
+                        'filepath': query[obj][q_img]
+                    })
+                    
+            for image in classes_shots[obj]:
+                for i in range(classes_shots[obj][image]):
+                    images.append({
+                        'dataset': dataset,
+                        'class_name': obj,
+                        'file_name': image,
+                        'filepath': os.path.join("data", dataset, "images" , obj, image)
+                    })
